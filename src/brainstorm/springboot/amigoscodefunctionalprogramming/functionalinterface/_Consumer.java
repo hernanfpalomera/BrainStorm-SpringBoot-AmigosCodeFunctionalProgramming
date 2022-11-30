6674275c9a1fa28026f2d66c01f2f090fb88feca
Hernan Fonseca Palomera <vn51wfb@email.wal-mart.com>
2022-11-30 11:33:28 -0600

    Adding _Consumer class with Consumer and BiConsumer functional interfaces

---
+++ src/brainstorm/springboot/amigoscodefunctionalprogramming/functionalinterface/_Consumer.java
@@ -0,0 +1,44 @@
+package brainstorm.springboot.amigoscodefunctionalprogramming.functionalinterface;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class _Consumer {
+
+    public static void main(String[] args) {
+
+        Customer maria = new Customer("Maria","9999");
+        greetCustomer(maria);
+        greetCustomerV2(maria,true);
+        greetCustomerV2(maria,false);
+
+        greetCustomerConsumer.accept(maria);
+        greetCustomerConsumerV2.accept(maria,true);
+        greetCustomerConsumerV2.accept(maria,false);
+
+    }
+
+    static BiConsumer<Customer,Boolean> greetCustomerConsumerV2 = (customer, showPhoneNumber) ->
+            System.out.println("Hello "+ customer.customerName + ", thanks for registering phone number " + (showPhoneNumber ? customer.customerPhoneNumber : "****"));
+
+    static Consumer<Customer> greetCustomerConsumer = customer ->
+            System.out.println("Hello "+ customer.customerName + ", thanks for registering phone number " + customer.customerPhoneNumber );
+
+    static void greetCustomer(Customer customer){
+        System.out.println("Hello "+ customer.customerName + ", thanks for registering phone number " + customer.customerPhoneNumber );
+    }
+
+    static void greetCustomerV2(Customer customer, Boolean showPhoneNumber){
+        System.out.println("Hello "+ customer.customerName + ", thanks for registering phone number " + (showPhoneNumber ? customer.customerPhoneNumber : "****"));
+    }
+
+    static class Customer{
+    private final String customerName;
+    private final String customerPhoneNumber;
+
+        public Customer(String customerName, String customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+}
